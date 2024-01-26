adb2bf9c97f42d0e02c780e805dea77fa97a5aa9
Nyirux <samuel47000@hotmail.com>
2024-01-26 13:52:02 +0100

    correction mobile

--- src/components/card_container/card_container.jsx
+++ src/components/card_container/card_container.jsx
@@ -11,17 +11,21 @@ function Cardcontainer() {
   useGSAP(() => {
     let sections = gsap.utils.toArray(".card");
 
-     gsap.to(sections, {
-        xPercent: -100 * (sections.length - 1),
-        ease: "none", 
-        scrollTrigger: {
-          trigger: ".containerCard",
-          pin: true,
-          scrub: 1,
-          snap: 1 / (sections.length -1)
-        }
-      });
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none", 
+      scrollTrigger: {
+        trigger: ".containerCard",
+        pin: true,
+        scrub: true,
+        invalidateOnRefresh: true,
+        anticipatePin: 1,
+        snap: 1 / (sections.length - 1),
+        touch: true 
+      }
+    });
   });
+
 
   return (
     <div className="containerCard">
@@ -29,41 +33,40 @@ function Cardcontainer() {
         title="Card 1"
         description="Description for Card 1"
         img="https://example.com/image1.jpg"  
-        link="https://github.com/WebDevPie/Smooth-Scrolling-For-Your-Website---Lenis-GSAP-Animation-ScrollTrigger/blob/master/style.css"
-      />
-      <Card
-        title="Card 1"
-        description="Description for Card 1"
-        img="https://example.com/image1.jpg"  
-        link="https://github.com/WebDevPie/Smooth-Scrolling-For-Your-Website---Lenis-GSAP-Animation-ScrollTrigger/blob/master/style.css"
-      />
-      <Card
-        title="Card 1"
-        description="Description for Card 1"
-        img="https://example.com/image1.jpg"  
-        link="https://github.com/WebDevPie/Smooth-Scrolling-For-Your-Website---Lenis-GSAP-Animation-ScrollTrigger/blob/master/style.css"
-      />
-      <Card
-        title="Card 1"
-        description="Description for Card 1"
-        img="https://example.com/image1.jpg"  
-        link="https://github.com/WebDevPie/Smooth-Scrolling-For-Your-Website---Lenis-GSAP-Animation-ScrollTrigger/blob/master/style.css"
+        link="https://example.com"
       />
       <Card
         title="Card 2"
         description="Description for Card 2"
         img="https://example.com/image2.jpg"  
-        link="https://github.com/WebDevPie/Smooth-Scrolling-For-Your-Website---Lenis-GSAP-Animation-ScrollTrigger/blob/master/style.css"
+        link="https://example.com"
       />
       <Card
         title="Card 3"
         description="Description for Card 3"
         img="https://example.com/image3.jpg"  
-        link="https://github.com/WebDevPie/Smooth-Scrolling-For-Your-Website---Lenis-GSAP-Animation-ScrollTrigger/blob/master/style.css"
+        link="https://example.com"
+      />
+      <Card
+        title="Card 3"
+        description="Description for Card 3"
+        img="https://example.com/image3.jpg"  
+        link="https://example.com"
+      />
+      <Card
+        title="Card 3"
+        description="Description for Card 3"
+        img="https://example.com/image3.jpg"  
+        link="https://example.com"
+      />
+      <Card
+        title="Card 3"
+        description="Description for Card 3"
+        img="https://example.com/image3.jpg"  
+        link="https://example.com"
       />
     </div>
   );
 }
 
 export default Cardcontainer;
-
